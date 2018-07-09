fbff2591975c4a62e04b722e54585fe953fc5df3
mtmtkj <mtmt101@usingstd.com>
2018-07-09 23:17:46 +0900

    Day003: TypeScript tutorial

---
+++ Day003/tutorial.ts
@@ -0,0 +1,68 @@
+// https://tutorialzine.com/2016/07/learn-typescript-in-30-minutes
+
+// 1. Primitives
+const
+	burger: string = 'hamburger',
+	calories: number = 300,
+	tasty: boolean = true
+
+function speak(food: string, energy: number): void {
+  console.log(`Our ${food} has ${energy} calories.`)
+}
+
+console.log('===== 1. Primitives =====')
+speak(burger, calories)
+
+// 2. Interface
+
+interface Food {
+    name: string
+    calories: number
+}
+
+function speak2(food: Food): void {
+  console.log(`Our ${food.name} has ${food.calories} calories.`);
+}
+
+const ice_cream = {
+  name: 'ice cream',
+  calories: 200,
+}
+
+console.log('===== 2. Interface =====')
+speak2(ice_cream)
+
+// 3. Class
+
+class Menu {
+	items: string[]
+	pages: number
+
+	constructor(items: string[], pages: number) {
+		this.items = items
+		this.pages = pages
+	}
+
+	show(): void {
+		console.log('Our menu for today:')
+		console.log(`${this.items.join(',')} in ${this.pages} page(s).`)
+	}
+}
+
+const sundayMenu = new Menu(['pancakes', 'waffles', 'orange juice'], 1)
+
+console.log('===== 3. Class =====')
+sundayMenu.show()
+
+class HappyMeal extends Menu {
+
+	show(): void {
+		console.log('Our special menu for children:')
+		console.log(`${this.items.join(',')} in ${this.pages} page(s).`)
+	}
+}
+
+const specialChildrenMenu = new HappyMeal(['candy', 'drink', 'toy'], 2)
+
+specialChildrenMenu.show()
+
