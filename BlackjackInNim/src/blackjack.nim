b3223000fcc8e07c31030f5d9bc5102945ce2932
nunulk <nunulk@usingstd.com>
2018-07-27 23:34:06 +0900

    Day020: Blackjack in Nim - implement player and take proc that move cards from deck to hand

--- BlackjackInNim/src/blackjack.nim
+++ BlackjackInNim/src/blackjack.nim
@@ -10,6 +10,14 @@ type Card = tuple[suit: Suit, value: int]
 
 type Deck = ref seq[Card]
 
+type Hand = seq[Card]
+
+type ObjPlayer = object
+  name: string
+  hand: Hand
+
+type Player = ref ObjPlayer
+
 proc `$`(deck: Deck): string =
   result = ""
   for c in deck[]:
@@ -31,9 +39,22 @@ proc newDeck(): Deck =
   deck[] = cards
   deck
 
+proc take(player: Player, deck: Deck, num: int) =
+  for n in 0..<num:
+    var c: Card = deck[].pop
+    player[].hand.add(c)
+
 proc main() = 
   var deck: Deck = newDeck()
+  var dealer = Player(name: "Dealder", hand: @[])
+  var player = Player(name: "You", hand: @[])
+
   echo $deck
+
+  take(dealer, deck, 2)
+  take(player, deck, 2)
+
+  echo $dealer[], $player[]
 
 when isMainModule:
    main()
